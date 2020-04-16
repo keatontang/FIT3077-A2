02c99bc75c450c80c84982ce4c6c4e2d9f06ae31
saratran <uyentran1479@gmail.com>
2020-04-16 16:28:07 +1000

    Add tutorial url

--- assignment2/src/main/java/com/fit3077/assignment2/Assignment2Application.java
+++ assignment2/src/main/java/com/fit3077/assignment2/Assignment2Application.java
@@ -3,6 +3,7 @@ package com.fit3077.assignment2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Tutorial: https://www.youtube.com/watch?v=vtPkZShrvXQ
 @SpringBootApplication
 public class Assignment2Application {
 
